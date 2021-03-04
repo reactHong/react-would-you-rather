f56bed76c5dadefb6fe9bafcbed5786b29bd2558
Hong Shik Kim <hong@hongs.dev>
2021-03-04 09:35:28 +0900

    feat: Sort Leader Board items

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -3,9 +3,20 @@ import LeaderBoardItem from './LeaderBoardItem';
 
 
 function LeaderBoard({ users }) {
+
+  const sortedUsers = Object.values(users);
+
+  sortedUsers.sort((user1, user2) => {
+    const user1Score = Object.values(user1.answers).length 
+                      + user1.questions.length;
+    const user2Score = Object.values(user2.answers).length 
+                      + user2.questions.length;
+    return user2Score - user1Score;
+  });
+
   return (
     <>
-      {Object.values(users).map(user => (
+      {sortedUsers.map(user => (
         <LeaderBoardItem key={user.id} user={user} />
       ))}
     </>
