935a3795a8d8bcfcc144106d5494b6fcd6e33e0e
Hong Shik Kim <hong@hongs.dev>
2021-03-13 05:54:31 +0900

    fix: Fix a bug that the unanswered questions are shown by default

--- src/reducer/preference.js
+++ src/reducer/preference.js
@@ -1,7 +1,13 @@
+import { SIGN_OUT } from "../actions/authedUser";
 import { SET_QUESTIONLIST_TABINDEX } from "../actions/shared";
 
 const preference = (state = {}, action) => {
   switch(action.type) {
+    case SIGN_OUT:
+      return {
+        ...state,
+        qListTabIndex: 0,
+      }
     case SET_QUESTIONLIST_TABINDEX:
       return {
         ...state,
