47773f31ea44556c7cf520d4403054ede64736f3
Hong Shik Kim <hong@hongs.dev>
2021-03-13 11:53:12 +0900

    feat: Add circular progressbar by react-circular-progressbar

--- src/components/PollCardResult.js
+++ src/components/PollCardResult.js
@@ -1,7 +1,24 @@
 import { connect } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+function ProgressBar({ percentage }) {
+  const text = `${percentage.toFixed(0)}%`;
+  return (
+    <CircularProgressbar 
+      value={percentage} 
+      text={text} 
+      styles={{
+        path: {
+          stroke: `#f96363dc`,
+        }
+      }}
+    />
+  );
+}
 
 function PollCardResult(props) {
-
   const { 
     authorName, 
     avatarURL, 
@@ -13,6 +30,18 @@ function PollCardResult(props) {
   const optionOneCount = optionOne.votes.length;
   const optionTwoCount = optionTwo.votes.length;
   const totalCount = optionOneCount + optionTwoCount;
+  const progress1 = optionOneCount / totalCount * 100;
+  const progress2 = optionTwoCount / totalCount * 100;
+  
+  const [progressOption1, setProgressOption1] = useState(0);
+  const [progressOption2, setProgressOption2] = useState(0);
+  
+  useEffect(() => {
+    setTimeout(() => {
+      setProgressOption1(progress1);
+      setProgressOption2(progress2);
+    }, 250);
+  }, [progress1, progress2]);
 
   return (
     <div className="cardContainer">
@@ -31,7 +60,9 @@ function PollCardResult(props) {
               <div>
                 Would you rather {optionOne.text}?
               </div>
-              <div>[Progress Bar]</div>
+              <div className="progressBar">
+                <ProgressBar percentage={progressOption1} />
+              </div>
               <div>
                 {optionOneCount} out of {totalCount} votes
               </div>
@@ -41,7 +72,9 @@ function PollCardResult(props) {
               <div>
                 Would you rather {optionTwo.text}?
               </div>
-              <div>[Progress Bar]</div>
+              <div className="progressBar">
+                <ProgressBar percentage={progressOption2} />
+              </div>
               <div>
                 {optionTwoCount} out of {totalCount} votes
               </div>
