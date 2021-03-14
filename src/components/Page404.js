f4449be54ae0bc4b798f90c928deb342c8b0048e
Hong Shik Kim <hong@hongs.dev>
2021-03-15 06:36:22 +0900

    feat: Go to Home page from Page404

--- src/components/Page404.js
+++ src/components/Page404.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
+import { Redirect } from "react-router-dom";
 
 function Page404() {
+  const [toHome, setToHome] = useState(false);
+
+  if (toHome) {
+    return <Redirect to="/" />
+  }
+
   return(
     <div className="errorPageContainer">
       <h1>Page Not Found</h1>
       <div>Unfortunately the page you're looking for doesn't exist (anymore) or there was an error in the link you followed or typed.</div>
       <div>
-        <button>Go to Homepage</button>
+        <button onClick={() => setToHome(true)}>Go to Homepage</button>
       </div>
     </div>
   );
